e7c01cd70a1da1da52181a071d85dd39da0b6893
Willem Nieuwenhuis <w.nieuwenhuis@utwente.nl>
2025-07-11 17:02:06 +0200

    Save captured images in a tree: station/year/month/day

--- src/camera/capture_functions.py
+++ src/camera/capture_functions.py
@@ -1,3 +1,4 @@
+from datetime import datetime, date
 from html import unescape
 import logging
 from pathlib import Path
@@ -123,8 +124,27 @@ def retrieve_image(img_url: str) -> bytes | None:
         return None
 
 
-def save_camera_image(img_data: bytes, img_filename: Path):
+def update_folder_tree(images_root: Path, station_name: str) -> Path:
+    ''' Images are saved using a hierarchy by station/year/month/day
+        This function ensures that the folder structure exists.
+
+        :param station_name: name of the station location for the tree
+    '''
+    today = date.today()
+    tree_path = images_root / station_name / str(today.year) / str(today.month) / str(today.day)
+    if not tree_path.exists():
+        tree_path.mkdir(parents=True, exist_ok=True)
+        logger.info(f"Created image folder: {tree_path}")
+
+    return tree_path
+
+
+def save_camera_image(img_data: bytes, images_root: Path, station: str, suffix: str) -> None:
     """Save the camera image to a file."""
+    timestamp = datetime.now().strftime("%Y%m%d_%H%M")
+    img_folder = update_folder_tree(images_root, station)
+    img_filename = img_folder / f"{station}_{timestamp}{suffix}"
+
     with open(img_filename, 'wb') as f:
         f.write(img_data)
     logger.info(f"Image saved as {img_filename}")
