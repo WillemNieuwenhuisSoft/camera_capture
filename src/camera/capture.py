cd2beaaac52dd434b77c8390460a7d4551338352
Willem Nieuwenhuis <w.nieuwenhuis@utwente.nl>
2025-07-23 11:41:30 +0200

    Removed magic numbers, pass result of builtins as parameters

--- src/camera/capture.py
+++ src/camera/capture.py
@@ -67,14 +67,13 @@ def capture_all():
         logger.info(f"Finished capturing image for {location}")
 
 
-def delay_to_next_capture_time(config: CameraConfig) -> tuple[int, datetime]:
+def delay_to_next_capture_time(config: CameraConfig, now: datetime) -> tuple[int, datetime]:
     """ Determine the initial start time based on the current time and the configured start time.
         Capture time is calculated at regular intervals since the start time.
         Example: If the start time is 06:30, the interval is 30 minutes and the current time is 07:13,
         the next capture will be at 07:30 (6:30 + 2 * 30).
         Return the seconds to wait before actually starting the capture.
     """
-    now = datetime.now()
     dt_start = now.replace(hour=config.start.hour, minute=config.start.minute, second=0, microsecond=0)
     if now.time() <= config.start:
         return (dt_start - now).seconds, dt_start
@@ -87,21 +86,22 @@ def delay_to_next_capture_time(config: CameraConfig) -> tuple[int, datetime]:
         # Otherwise, return the next interval after the current time
         periods = (now - dt_start).seconds // (config.interval * 60)
         remain = (now - dt_start).seconds % (config.interval * 60)
-        if remain > 0 and remain < config.interval * 60:
+        if (remain > 0) and (remain < config.interval * 60):
+            # last period, may be less then the interval, so adjust
             periods += 1
         target = dt_start + timedelta(minutes=(periods) * config.interval)
         return (target - now).seconds, target
 
 
-def wait_until_next_capture(seconds: int):
+def wait_until_next_capture(seconds: int, period_length: int = 3600):
     """
         Wait until the next capture time, allowing for keyboard interrupts.
         Once per hour report remaining time.
     """
-    period = 0
-    periods = seconds // 3600
+    periods = (seconds // period_length) + 1
+    period = periods
     while seconds > 0:
-        to_wait = min(seconds+1, 3600)
+        to_wait = min(seconds, period_length)
         try:
             st = time.time()
             if periods > 1:
@@ -113,21 +113,23 @@ def wait_until_next_capture(seconds: int):
         if periods > 1:
             print(f"Completed sleep period {period + 1} of {periods}.")
         seconds -= to_wait
-        period += 1
+        period -= 1
 
 
 def capture_all_repeat(capture_mode: int = CAPTURE_TODAY):
     config = CameraConfig()
-    day_end = datetime.now().replace(hour=config.end.hour, minute=config.end.minute, second=0, microsecond=0)
+    now = datetime.now()
+    period_length = 3600
+    day_end = now.replace(hour=config.end.hour, minute=config.end.minute, second=0, microsecond=0)
     try:
         while True:
             capture_all()
-            sleep_time, capture_time = delay_to_next_capture_time(config)
+            sleep_time, capture_time = delay_to_next_capture_time(config, now)
             if (capture_mode == CAPTURE_TODAY) and capture_time > day_end:
                 logger.info("Capture finished for today.")
                 break
             logger.info(f'Next capture at {capture_time}; Press Ctrl+C to stop.')
-            wait_until_next_capture(sleep_time)
+            wait_until_next_capture(sleep_time, period_length)
     except KeyboardInterrupt:
         logger.info("Stopping repeat capture.")
     except EndCaptureException:
